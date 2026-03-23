c9f8ba47ce3b0d04e5d4527ebd8b769a4bc2fde5
HeloisaVictoriaSilva <163949603+HeloisaVictoriaSilva@users.noreply.github.com>
2026-03-23 12:47:10 -0300

    Atividade 02 -  Migração da API de FASTAPI para FLASK
    Atividade 02 -  Migração da API de FASTAPI para FLASK

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+logins = {
+    "Heloisa": "1206",
+    "Victoria": "1304",
+    "Cas": "1608"
+}
+
+@app.route('/')
+def root():
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if username in logins and logins[username] == password:
+            return redirect(url_for('home'))
+        else:
+            return render_template('login.html', error="erro no login")
+    return render_template('login.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/logout')
+def logout():
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
