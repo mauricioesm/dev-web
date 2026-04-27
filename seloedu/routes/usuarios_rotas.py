9a08539dfd67a45809dbe25580616ed6708c4285
Matheus-TPF <matheus.jose@tpfe.com.br>
2026-04-27 11:49:19 -0300

    Atividade 07: Crud de usuários do sistema

--- seloedu/routes/usuarios_rotas.py
+++ seloedu/routes/usuarios_rotas.py
@@ -1,9 +1,12 @@
 from flask import Blueprint
 from flask_login import login_required
 
-from views.usuarios_views import detalhes_usuario, listar_usuarios
+from views.usuarios_views import detalhes_usuario, listar_usuarios, modificar_usuario, deletar_usuario, criar_usuario
 
 usuarios_bp = Blueprint("usuario", __name__)
 
 usuarios_bp.add_url_rule("/users",          view_func=login_required(listar_usuarios),  endpoint="listar",   methods=["GET"])
 usuarios_bp.add_url_rule("/users/<int:id>", view_func=login_required(detalhes_usuario), endpoint="detalhes", methods=["GET"])
+usuarios_bp.add_url_rule("/users/modificar-usuario/<int:id>", view_func=login_required(modificar_usuario), endpoint="modificar", methods=["POST", "GET"])
+usuarios_bp.add_url_rule("/users/deletar/<int:id>", view_func=login_required(deletar_usuario), endpoint="deletar", methods=["POST", "GET"])
+usuarios_bp.add_url_rule("/users/criar/", view_func=login_required(criar_usuario), endpoint="criar", methods=["POST", "GET"])
--- seloedu/views/usuarios_views.py
+++ seloedu/views/usuarios_views.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for, flash
 from models.usuario_models import Usuario
+from extensions import db
 
 
 def listar_usuarios():
@@ -10,3 +11,75 @@ def listar_usuarios():
 def detalhes_usuario(id):
     usuario = Usuario.query.get_or_404(id)
     return render_template("usuario/detalhe_usuario.html", usuario=usuario)
+
+def modificar_usuario(id):
+
+    usuario = Usuario.query.get_or_404(id)
+
+    if not usuario:
+        return render_template("usuario/editar_usuario.html")
+    
+    if request.method == "POST":
+
+        novo_nome = request.form.get("nome").strip()
+        nova_funcao = request.form.get("funcao").strip()
+
+        usuario.nome  = novo_nome
+        usuario.funcao = nova_funcao
+    
+        db.session.commit()
+
+        return redirect(url_for("usuario.detalhes", id=usuario.id))
+       
+
+    return render_template("usuario/modificar_usuario.html", usuario=usuario)
+
+
+def deletar_usuario(id):
+
+    usuario = Usuario.query.get_or_404(id)
+
+    if not usuario:
+        flash("Usuário não encontrado.", "error")
+        return redirect(url_for("usuario.listar"))
+
+    if request.method == "POST":
+        
+        usuario_id = request.form.get("id").strip()
+        
+        db.session.delete(usuario)
+        db.session.commit()
+
+        return redirect(url_for("usuario.listar"))
+
+    return render_template("usuario/deletar_usuario.html", usuario=usuario)
+
+
+def criar_usuario():
+
+    if request.method == "POST":
+        nome = request.form.get("nome").strip()
+        email = request.form.get("email").strip()
+        senha = request.form.get("senha")
+        funcao = request.form.get("funcao").strip()
+
+        if Usuario.query.filter_by(email=email).first():
+            flash("Email já cadastrado. Por favor, use outro email.", "error")
+
+        usuario = Usuario(
+            nome = nome,
+            email = email,
+            senha_hash = senha,
+            funcao = funcao,
+        )
+
+        try:
+            db.session.add(usuario)
+            db.session.commit()
+            return render_template("usuario/detalhe_usuario.html", usuario=usuario)
+        except Exception as e:
+            db.session.rollback()
+            return render_template("usuario/criar_usuario.html")
+
+    return render_template("usuario/criar_usuario.html")
+
